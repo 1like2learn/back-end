cb731dc151a5d926db6b3b168c50c30aada467e1
1like2learn <ndespained@gmail.com>
2020-08-24 10:10:16 -0700

    User model added to project

---
+++ airbnbapi/src/main/java/com/lambdaschool/airbnb/airbnbapi/AirbnbapiApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.airbnb.airbnbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbapiApplication.class,
+            args);
+    }
+
+}
